837deb43b670c04e87f714b2f475eec6da5f53af
TheBakaPotato <TheBakaPotato@thebakapotato.com>
2021-12-15 23:39:01 -0300

    TesteUser2

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -9,6 +9,13 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
     Button btn_GetCityID, btn_getWeatherByCityID, btn_getWeatherByCityName;
     EditText et_DataInput;
@@ -26,7 +33,30 @@ public class MainActivity extends AppCompatActivity {
         btn_GetCityID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Botão 1", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "Botão 1", Toast.LENGTH_SHORT).show();
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url ="https://www.metaweather.com/api/location/search/?query=london";
+
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                                //textView.setText("Response is: "+ response.substring(0,500));
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Algo deu errado my brodi", Toast.LENGTH_SHORT).show();
+                        //textView.setText("That didn't work!");
+                    }
+                });
+
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
 
             }
         });
